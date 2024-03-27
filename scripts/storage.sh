3846d1cec053ced26764df7764d60ba10b11f87d
ramisaalam1 <159954926+ramisaalam1@users.noreply.github.com>
2024-03-26 22:24:35 -0400

    Initial Commit

--- scripts/storage.sh
+++ scripts/storage.sh
@@ -1,3 +1,3 @@
 gcloud storage buckets create gs://cis4400-hw-ra --project=silver-bird-412916 --default-storage-class=STANDARD --location=us-central1 --uniform-bucket-level-access
 curl -o data.json https://data.cityofnewyork.us/resource/uip8-fykc.json
-
+gsutil cp data.json gs://cis4400-hw-ra
