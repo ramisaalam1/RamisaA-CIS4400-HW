9c99c9f46ea5f7d3e13f3f192404e616a080bfd8
ramisaalam1 <159954926+ramisaalam1@users.noreply.github.com>
2024-04-25 21:19:38 -0400

    commit

--- scripts/storage.py
+++ scripts/storage.py
@@ -1,3 +1,30 @@
-gcloud storage buckets create gs://cis4400-hw-ra --project=silver-bird-412916 --default-storage-class=STANDARD --location=us-central1 --uniform-bucket-level-access
-curl -o data.json https://data.cityofnewyork.us/resource/uip8-fykc.json
-gsutil cp data.json gs://cis4400-hw-ra
+import os
+from google.cloud import storage
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/ramisaalam/Downloads/silver-bird-412916-cd24855ad9db.json'
+
+storage_client = storage.Client()
+
+print("Buckets:")
+for bucket in storage_client.list_buckets():
+    print(bucket.name)
+    
+#Storage
+import requests
+from google.cloud import storage
+
+url = 'https://data.cityofnewyork.us/resource/uip8-fykc.csv'
+
+response = requests.get(url)
+data = response.text  
+
+bucket_name = 'cis4400-hw-ra'
+file_name = 'nypd_arrest_data.csv'  
+
+storage_client = storage.Client()
+bucket = storage_client.bucket(bucket_name)
+
+blob = bucket.blob(file_name)
+blob.upload_from_string(data, content_type='text/csv')
+
+print(f'File uploaded to {bucket_name}/{file_name}')
